56b1f7a0ff4838de22a9772639c49b6edfa3b366
RChamyGit <39716460+RChamyGit@users.noreply.github.com>
2023-09-25 16:36:13 -0400

    api commit

---
+++ api/java-restful-challenge/src/test/java/com/studies/java/javarestfulchallenge/JavaRestfulChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studies.java.javarestfulchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestfulChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
